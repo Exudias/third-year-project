81cf955efb68e5b8d2628f9df52c3442a50ecf77
f77433es <eduard.sarkisyan@student.manchester.ac.uk>
2023-10-08 12:38:03 +0100

    Create FollowTransform script (temporary), begin spirit implementation

---
+++ Assets/Code/FollowTransform.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    [SerializeField] private Transform followTransform;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private bool ignoreZ = true;
+
+    private void LateUpdate()
+    {
+        if (followTransform == null) return;
+
+        Vector3 newPos = followTransform.position + offset;
+        
+        if (ignoreZ)
+        {
+            newPos.z = transform.position.z;
+        }
+
+        transform.position = newPos;
+    }
+
+    public void UpdateFollowTransform(Transform newFollow)
+    {
+        followTransform = newFollow;
+    }
+}
