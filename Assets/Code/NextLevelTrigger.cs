ffba17ff3cfd30d39f74966046b26ffff23ddbd5
f77433es <eduard.sarkisyan@student.manchester.ac.uk>
2023-10-29 21:23:37 +0000

    Remove debug text that's not needed

--- Assets/Code/NextLevelTrigger.cs
+++ Assets/Code/NextLevelTrigger.cs
@@ -4,7 +4,6 @@ public class NextLevelTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("mdq");
         bool collisionIsPlayer = collision.gameObject.GetComponent<PlayerLogic>() != null;
         if (collisionIsPlayer)
         {
