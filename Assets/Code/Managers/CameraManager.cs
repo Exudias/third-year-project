6254bd9e61a67dcef4595cb4f54e41a11a11c7e7
f77433es <eduard.sarkisyan@student.manchester.ac.uk>
2023-11-23 14:24:15 +0000

    Create 2nd bulb shader, fix pp camera

--- Assets/Code/Managers/CameraManager.cs
+++ Assets/Code/Managers/CameraManager.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class CameraManager : MonoBehaviour
 {
