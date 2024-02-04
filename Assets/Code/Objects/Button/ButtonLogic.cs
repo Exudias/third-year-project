36964cb3c58846b6c5b93f7552f17e40a68a0a92
f77433es <eduard.sarkisyan@student.manchester.ac.uk>
2024-02-04 13:56:17 +0000

    Create application icon, name game, lower button sounds

--- Assets/Code/Objects/Button/ButtonLogic.cs
+++ Assets/Code/Objects/Button/ButtonLogic.cs
@@ -6,15 +6,17 @@ public class ButtonLogic : MonoBehaviour
     [SerializeField] private AudioClip pressSound;
     [SerializeField] private AudioClip unpressSound;
 
+    private const float SOUND_VOLUME = 0.5f;
+
     public void OnButtonPressed()
     {
         activateable.GetComponent<IActivatable>().Activate();
-        AudioSource.PlayClipAtPoint(pressSound, transform.position, PlayerPrefs.GetFloat("soundVolume", 1f));
+        AudioSource.PlayClipAtPoint(pressSound, transform.position, PlayerPrefs.GetFloat("soundVolume", 1f) * SOUND_VOLUME);
     }
 
     public void OnButtonUnpressed()
     {
         activateable.GetComponent<IActivatable>().Deactivate();
-        AudioSource.PlayClipAtPoint(unpressSound, transform.position, PlayerPrefs.GetFloat("soundVolume", 1f));
+        AudioSource.PlayClipAtPoint(unpressSound, transform.position, PlayerPrefs.GetFloat("soundVolume", 1f) * SOUND_VOLUME);
     }
 }
