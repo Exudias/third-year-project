d37e4368f63f9bd1cfed927b0b8cbcacfe0df6ab
f77433es <eduard.sarkisyan@student.manchester.ac.uk>
2024-02-10 13:00:05 +0000

    Timer now properly placed based on black bars

--- Assets/Code/UI/TimerLogic.cs
+++ Assets/Code/UI/TimerLogic.cs
@@ -1,13 +1,44 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class TimerLogic : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private RectTransform rectTransform;
+
+    private Camera mainCam;
+    private RectTransform parentRect;
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+        parentRect = transform.parent.GetComponent<RectTransform>();
+    }
 
     private void Update()
     {
         timerText.text = TimeToString(SpeedrunManager.time);
+        SyncToBlackBars();
+    }
+
+    private void SyncToBlackBars()
+    {
+        // Get canvas size
+        float uiWidth = parentRect.rect.width;
+        float uiHeight = parentRect.rect.height;
+        // Calculate X displacement
+        float verticalBlackBars = Screen.width - mainCam.pixelWidth;
+        float singleBlackBarWidth = verticalBlackBars / 2;
+        float singleBlackBarPortionOfScreenX = singleBlackBarWidth / Screen.width;
+        float timerDisplacementX = singleBlackBarPortionOfScreenX * uiWidth;
+        // Calculate Y displacement
+        float horizontalBlackBars = Screen.height - mainCam.pixelHeight;
+        float singleBlackBarHeight = horizontalBlackBars / 2;
+        float singleBlackBarPortionOfScreenY = singleBlackBarHeight / Screen.height;
+        float timerDisplacementY = singleBlackBarPortionOfScreenY * uiHeight;
+        // Apply displacement
+        rectTransform.anchoredPosition = new Vector3(timerDisplacementX, -timerDisplacementY, 0);
     }
 
     private string TimeToString(float time)
