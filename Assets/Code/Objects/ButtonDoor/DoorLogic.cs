baab5a356fdba7411ccc3f9ce5590f28f8d7ab2a
f77433es <eduard.sarkisyan@student.manchester.ac.uk>
2024-02-13 12:24:12 +0000

    Add cables for buttons

--- Assets/Code/Objects/ButtonDoor/DoorLogic.cs
+++ Assets/Code/Objects/ButtonDoor/DoorLogic.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class DoorLogic : MonoBehaviour, IActivatable
 {
+    [Header("References")]
     [SerializeField] private Transform door1;
     [SerializeField] private Transform door2;
     [SerializeField] private PlayerDetectorTrigger detector;
+    [Header("Parameters")]
     [SerializeField] private float openTime;
     [SerializeField] private float closeTime;
     [SerializeField] private float openDistance;
+    [Header("Visuals")]
     [SerializeField] private SpriteRenderer counterSprite;
+    [SerializeField] private GameObject cableHolder;
+    [SerializeField] private Color cableOffColor;
+    [SerializeField] private Color cableOnColor;
 
     private bool active;
     private bool vertical;
@@ -23,6 +30,8 @@ public class DoorLogic : MonoBehaviour, IActivatable
     private Vector2 door2ClosedPosition;
     private Vector2 door2OpenPosition;
 
+    private List<SpriteRenderer> cableFGSprites;
+
     private void Start()
     {
         active = false;
@@ -35,7 +44,27 @@ public class DoorLogic : MonoBehaviour, IActivatable
         door1ClosedPosition = door1.position;
         door2ClosedPosition = door2.position;
         door1OpenPosition = door1ClosedPosition + (vertical ? Vector2.up : Vector2.left) * openDistance; 
-        door2OpenPosition = door2ClosedPosition + (vertical ? Vector2.down : Vector2.right) * openDistance; 
+        door2OpenPosition = door2ClosedPosition + (vertical ? Vector2.down : Vector2.right) * openDistance;
+
+        cableFGSprites = GetFGCables();
+    }
+
+    private List<SpriteRenderer> GetFGCables()
+    {
+        List<SpriteRenderer> fgCables = new List<SpriteRenderer>();
+
+        // If no cables, return empty list
+        if (cableHolder == null) return fgCables;
+
+        int cableCount = cableHolder.transform.childCount;
+        for (int i = 0; i < cableCount; i++)
+        {
+            Transform cable = cableHolder.transform.GetChild(i);
+            // Get child of cable, which is the actual sprite of the cable's "energy"
+            fgCables.Add(cable.GetChild(0).GetComponent<SpriteRenderer>());
+        }
+
+        return fgCables;
     }
 
     private void Update()
@@ -47,7 +76,15 @@ public class DoorLogic : MonoBehaviour, IActivatable
 
     private void UpdateVisuals()
     {
+        // Circle counter
         counterSprite.material.SetFloat("_FillPercent", percentOpen);
+        // Button cables
+        Color cableColor = Color.Lerp(cableOffColor, cableOnColor, percentOpen);
+        for (int i = 0; i < cableFGSprites.Count; i++)
+        {
+            SpriteRenderer cable = cableFGSprites[i];
+            cable.color = cableColor;
+        }
     }
 
     private void ApplyOpen()
